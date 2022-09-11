31082887688cb66bb631a88d4c07f533b5f15404
Zzz <zkmd2012@inbox.ru>
2022-09-11 20:43:08 +0200

    BUG - button should be clicked twice

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 import Keyboard from './components/Keyboard/Keyboard'
 import Task from './components/Task/Task'
@@ -8,17 +8,30 @@ import { symbols } from './data/symbols'
 function App() {
   const secretInputRef = useRef()
 
-  const [task, setTask] = useState('I am batman!')
+  const [task, setTask] = useState('i am batman!')
   const [currentSymbol, setCurrentSymbol] = useState(0)
   const [currentCharCode, setCurrentCharCode] = useState(
     task.charCodeAt(currentSymbol)
   )
 
+  const [inputCode, setInputCode] = useState(currentCharCode)
+
+  useEffect(() => {
+    secretInputRef.current.focus()
+  }, [])
+
+  // useEffect(() => {
+  //   // console.log(inputCode)
+
+  //   if (inputCode === currentCharCode) {
+  //     console.log('hi', inputCode)
+  //   }
+  // }, [inputCode])
+
   const next = () => {
     setCurrentSymbol((prev) => {
       prev++
       if (prev === task.length) {
-        alert('Finish')
         prev = 0
         setCurrentCharCode(task.charCodeAt(prev))
         return prev
@@ -26,15 +39,26 @@ function App() {
       setCurrentCharCode(task.charCodeAt(prev))
       return prev
     })
-    // setCurrentCharCode(task.charCodeAt(currentSymbol))
   }
-
-  console.log(currentCharCode)
-  secretInputRef.current.focus()
 
   const getFocusBackToInput = () => {
     secretInputRef.current.focus()
   }
+
+  const onKeyDownHandler = (e) => {
+    let code = secretInputRef.current.value.charCodeAt(0)
+
+    // if ((code >= 97 && code <= 122) || (code >= 65 && code >= 90)) {}
+    setInputCode(code)
+
+    if (code === currentCharCode) {
+      next()
+    }
+
+    secretInputRef.current.value = ''
+  }
+
+  console.log('rerender')
 
   return (
     <div className='App' onClick={getFocusBackToInput}>
@@ -42,7 +66,11 @@ function App() {
       <Keyboard symbols={symbols} currentCharCode={currentCharCode} />
 
       <button onClick={next}>Test button</button>
-      <input type='text' ref={secretInputRef}></input>
+      <input
+        type='text'
+        ref={secretInputRef}
+        onKeyDown={onKeyDownHandler}
+      ></input>
     </div>
   )
 }
