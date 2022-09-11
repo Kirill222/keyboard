2a878ac94301a939ad9ea57ffbb8c2292386fe91
Zzz <zkmd2012@inbox.ru>
2022-09-11 09:33:36 +0200

    All keys added

--- src/components/Keyboard/Key.jsx
+++ src/components/Keyboard/Key.jsx
@@ -9,6 +9,47 @@ function Key({ symbol }) {
     )
   }
 
+  if (symbol.type === 'bigthree') {
+    return (
+      <div className={`${s.key} ${s.bigthree} ${s.special}`}>{symbol.main}</div>
+    )
+  }
+
+  if (symbol.type === 'bigfour') {
+    return (
+      <div className={`${s.key} ${s.bigfour} ${s.special}`}>{symbol.main}</div>
+    )
+  }
+
+  if (symbol.type === 'ctrl') {
+    return (
+      <div className={`${s.key} ${s.ctrl} ${s.special}`}>{symbol.main}</div>
+    )
+  }
+
+  if (symbol.type === 'option') {
+    return (
+      <div className={`${s.key} ${s.option} ${s.special}`}>{symbol.main}</div>
+    )
+  }
+
+  if (symbol.type === 'space') {
+    return (
+      <div className={`${s.key} ${s.space} ${s.special}`}>{symbol.main}</div>
+    )
+  }
+
+  if (symbol.type === 'empty') {
+    return (
+      <div className={`${s.key} ${s.empty} ${s.special}`}>{symbol.main}</div>
+    )
+  }
+
+  // switch (symbol.type) {
+  //   case 'bigonetwo':
+
+  // }
+
   if (symbol.shift) {
     return (
       <div className={`${s.key}`}>
--- src/data/symbols.js
+++ src/data/symbols.js
@@ -113,4 +113,124 @@ export const symbols = [
     shift: '|',
     main: '\\',
   },
+  {
+    type: 'bigthree',
+    main: 'caps lock',
+  },
+  {
+    lower: 'a',
+    upper: 'A',
+  },
+  {
+    lower: 's',
+    upper: 'S',
+  },
+  {
+    lower: 'd',
+    upper: 'D',
+  },
+  {
+    lower: 'f',
+    upper: 'F',
+  },
+  {
+    lower: 'g',
+    upper: 'G',
+  },
+  {
+    lower: 'h',
+    upper: 'H',
+  },
+  {
+    lower: 'j',
+    upper: 'J',
+  },
+  {
+    lower: 'k',
+    upper: 'K',
+  },
+  {
+    lower: 'l',
+    upper: 'L',
+  },
+  {
+    shift: ':',
+    main: ';',
+  },
+  {
+    shift: '"',
+    main: "'",
+  },
+  {
+    type: 'bigthree',
+    main: 'enter',
+  },
+  {
+    type: 'bigfour',
+    main: 'shift',
+  },
+  {
+    lower: 'z',
+    upper: 'Z',
+  },
+  {
+    lower: 'x',
+    upper: 'X',
+  },
+  {
+    lower: 'c',
+    upper: 'C',
+  },
+  {
+    lower: 'v',
+    upper: 'V',
+  },
+  {
+    lower: 'b',
+    upper: 'B',
+  },
+  {
+    lower: 'n',
+    upper: 'N',
+  },
+  {
+    lower: 'm',
+    upper: 'M',
+  },
+  {
+    shift: '<',
+    main: ',',
+  },
+  {
+    shift: '>',
+    main: '.',
+  },
+  {
+    shift: '?',
+    main: '/',
+  },
+  {
+    type: 'bigfour',
+    main: 'shift',
+  },
+  {
+    type: 'ctrl',
+    main: 'control',
+  },
+  {
+    type: 'option',
+    main: 'option',
+  },
+  {
+    type: 'space',
+    main: 'space',
+  },
+  {
+    type: 'option',
+    main: 'option',
+  },
+  {
+    type: 'empty',
+    main: '',
+  },
 ]
